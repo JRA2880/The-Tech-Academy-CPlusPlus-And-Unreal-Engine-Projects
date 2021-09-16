35f552e8dea52684ec7fe547342fed820d957549
JRA2880 <johnrusselladamsjr@gmail.com>
2021-09-16 00:18:20 -0400

    Update to GitHub

---
+++ Small/Small/Small.cpp
@@ -0,0 +1,46 @@
+// Small.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using std::cout;
+
+double add(double x, double y);
+
+double add(double a, double b, double c);
+
+bool test(bool x);
+
+bool test(double x);
+
+int main()
+{
+    int total = add(3.0, 4.0);
+    cout << "3 + 4 is " << total << '\n';
+
+    double another = add(1.2, 3.4);
+    cout << '\n';
+    cout << "1.2 + 3.4 is " << another;
+    cout << '\n'; 
+
+    auto totalofthree = add(1.1, 2.2, 3.3);
+    cout << "1.1 + 2.2 + 3.3 is " << totalofthree;
+    cout << '\n';
+
+    add(0, 0, 0);
+
+    if (test(true))
+    {
+        cout << "true passes the test" << '\n';
+    } 
+    if (test(3.2))
+    {
+        cout << "3.2 passes the test" << '\n';
+    } 
+    if (test(3.0))
+    {
+        cout << "3 passes the test" << '\n';
+    }
+    return 0;
+}
+
