0fbc6d55c1749c9ea9fd8228aa2249e4a1e99cb8
JRA2880 <44944337+JRA2880@users.noreply.github.com>
2021-10-18 22:11:35 -0400

    Delete PlaguePrototype directory
    Deleting PlaguePrototype to move it to its own repository.

--- PlaguePrototype/Intermediate/Build/Win64/PlaguePrototypeEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30136
-#include "D:/The Tech Academy - Game Developer Boot Camp/Unreal Projects/PlaguePrototype/Intermediate/Build/Win64/PlaguePrototypeEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
